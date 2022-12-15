443049fc53f28efcaf471d6187eea7de1c501804
KeMing <KeMing@LAPTOP-N8CEPPHA>
2022-12-16 02:36:14 +0800

    Add project files.

---
+++ EventRegistration/Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using EventRegistration.Server.Data;
+using EventRegistration.Server.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(EventRegistration.Server.Areas.Identity.IdentityHostingStartup))]
+namespace EventRegistration.Server.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
